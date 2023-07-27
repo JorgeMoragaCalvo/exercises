80b52179a4c9e2c1065564716f6de4007f307f44
JorgeMoragaCalvo <jorge.moraga.c@usach.cl>
2023-07-27 13:05:21 -0400

    description Armstrong numbers

--- c/digits.c
+++ c/digits.c
@@ -2,6 +2,13 @@
 #include <stdlib.h>
 #include <math.h>
 
+/*
+    An Armstrong number is a number that is the sum of its own digits each raised to the power of the number of digits.
+    For example:
+    153 is an Armstrong number, because: 153 = 1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153
+*/
+
+
 int digits(int);
 int *arr_digits(int);
 int armstrong(int);
